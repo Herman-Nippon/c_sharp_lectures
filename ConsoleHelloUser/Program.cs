0256069755c016906e4cc6b61b0159935e54ca62
Herman <verypuzikov2004@gmail.com>
2023-05-18 15:06:24 +0500

    add personal response

--- ConsoleHelloUser/Program.cs
+++ ConsoleHelloUser/Program.cs
@@ -1,4 +1,12 @@
 ﻿Console.Write("Enter your name: ");
 string username = Console.ReadLine();
-Console.Write("Hello, ");
-Console.Write(username);
+
+if (username.ToLower() == "herman")
+{
+    Console.WriteLine("Hello, mate, how's it going?");
+}
+else 
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
