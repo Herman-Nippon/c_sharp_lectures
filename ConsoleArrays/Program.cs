5f04f5ffff23801c29ccf447628e7fe43e0d9c4a
Herman <verypuzikov2004@gmail.com>
2023-05-22 17:42:14 +0500

    add ConsoleArrays

---
+++ ConsoleArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; ++i)
+    {
+        array[i] = new Random().Next(0, 11);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; ++i)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = new int[10];
+PrintArray(array);
+FillArray(array);
+PrintArray(array);
