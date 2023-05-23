48a6998b47f2464e45c7699750f5fa389002b85e
Herman <verypuzikov2004@gmail.com>
2023-05-23 07:47:48 +0500

    add Bubble Sort algo

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,48 @@
+﻿void PrintArray(int[] array)
+{
+    foreach (int i in array)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int length, int[] array)
+{
+    for (int i = 0; i < length; ++i)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+
+void BubbleSort(int length, int[] array)
+{
+    for (int iteration = 0; iteration < length - 1; ++iteration)
+    {
+        bool swapped = false;
+        for (int i = 0; i < length - iteration - 1; ++i)
+        {
+            if (array[i] > array[i + 1])
+            {
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped)
+        {
+            Console.WriteLine($"The algo stopped at iteration {iteration + 1} of {length}");
+            break;
+        }
+    }
+}
+
+int[] array = new int[20];
+int length = array.Length;
+FillArray(length, array);
+PrintArray(array);
+
+BubbleSort(length, array);
+PrintArray(array);
+
